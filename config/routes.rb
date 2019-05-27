dd1ab4ed974e63d3b003a4b109043f731af63e93
ssatyaji <msatyaji31@gmail.com>
2019-05-28 04:51:55 +0700

    Add viewport

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "forum_threads#index"
+  root "forum_threads#index"
 
   resources :forum_threads, only: [:show, :new, :create, :edit, :update, :destroy] do
     put :pinit, on: :member
